ea2e5d3b6336272b4cbd928c85e7291c66119fc7
masteroftime <martinfeiler@hotmail.com>
2024-01-18 11:42:05 +0100

    Added ability to throw objects

--- src/player.gd
+++ src/player.gd
@@ -1,6 +1,9 @@
 extends Node3D
 
 const MOUSE_SENSITIVITY = 0.003
+const THROW_SPEED = 2
+
+const SymbolX = preload("res://src/symbol_x.tscn")
 
 @onready var camera = $Camera3D
 
@@ -12,4 +15,8 @@ func _unhandled_input(event):
 		rotation.y = clamp(rotation.y - event.relative.x * MOUSE_SENSITIVITY, -PI/4, PI/4)
 		camera.rotation.x = clamp(camera.rotation.x-event.relative.y * MOUSE_SENSITIVITY, -PI/4, PI/4)
 		
-		
+	if event.is_action_pressed("throw"):
+		var symbol = SymbolX.instantiate()
+		symbol.position = global_position
+		symbol.linear_velocity = -basis.z * THROW_SPEED
+		get_parent_node_3d().add_child(symbol)
